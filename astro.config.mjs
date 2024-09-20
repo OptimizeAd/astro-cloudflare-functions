e8b0c7f5c89c9d7cf67dec45f108dd673cdd2d51
Alex Beck <alex@optimize.ad>
2024-09-20 15:44:06 -0500

    not detecting functions folder

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: cloudflare({
+    mode: 'directory',
+    platformProxy: {
+      enabled: true
+    },
+  })
+});
